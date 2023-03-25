dcd4a54b1a9df6fe104f3a97cc28a50a2ab30a59
lolEstraski <carlos1andres20@gmail.com>
2023-03-25 13:31:32 -0500

    se hace configuracion primaria

---
+++ Negocio/src/main/java/co/edu/uniquindio/tPublico/NegocioApplication.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.tPublico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NegocioApplication.class, args);
+    }
+
+}
+
+
